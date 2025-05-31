cb7889c7fd81dc11ffa4cd575ed9a25a405887d0
a-tomlin <110063128+a-tomlin@users.noreply.github.com>
2025-05-31 15:09:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ fetch('WiFi.geojson')
         color: 'darkblue',
         weight: 1,
         fillOpacity: 0.7
-      }).bindPopup(feature.properties?.DESCRIPT ||)
+      }).bindPopup(feature.properties?.DESCRIPT ||'Description');
     }).addTo(map);
   });
 
